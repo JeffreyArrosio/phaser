bc0e64e71ad5cfc8e8b6df3f7c010df6df102d3d
JeffreyArrosio <jeffrey.aj98@gmail.com>
2025-02-09 22:09:52 +0000

    Bici anti CORS

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,11 +1,18 @@
 import { Scene } from 'phaser';
 
 export class Game extends Scene {
+
+
     constructor() {
         super('Game');
     }
 
-    create() {
+    async create() {
+        
+        let bicis = await getBicis("https://a24expressgen.onrender.com/api/bicicletas")
+        console.log(bicis);
+        let bici = bicis.bicicletas[0]
+
         this.cameras.main.setBackgroundColor(0x00ff00);
 
         this.add.image(512, 384, 'background').setAlpha(0.5);
@@ -21,34 +28,42 @@ export class Game extends Scene {
             start.destroy()
             this.add.image(500, 400, 'bwipo').setOrigin(0.5);
             this.add.text(300, 200, '*Clic para continuar*',)
-            let text1 = this.add.text(512, 584, 'Bienvenido a phaser', {
+            let text1 = this.add.text(512, 584, 'Bienvenido a phaser.', {
                 fontSize: 38, color: '#ffffff',
                 stroke: '#000000', strokeThickness: 8,
                 align: 'center'
             }).setOrigin(0.5);
             this.input.once('pointerdown', () => {
                 text1.destroy()
-                text1 = this.add.text(512, 584, 'Vamos a comenzar con lo básico de \nphaser para esta prctica', {
+                text1 = this.add.text(512, 584, 'Vamos a comenzar con lo básico de \nphaser para esta prctica.', {
                     fontSize: 38, color: '#ffffff',
                     stroke: '#000000', strokeThickness: 8,
                     align: 'center'
                 }).setOrigin(0.5);
                 this.input.once('pointerdown', () => {
                     text1.destroy()
-                    text1 = this.add.text(512, 584, 'Para ello, se ha hecho esta pequeña \nanimación para la actividad', {
+                    text1 = this.add.text(512, 584, 'Para ello, se ha hecho esta pequeña \nanimación para la actividad.', {
                         fontSize: 38, color: '#ffffff',
                         stroke: '#000000', strokeThickness: 8,
                         align: 'center'
                     }).setOrigin(0.5);
                     this.input.once('pointerdown', () => {
                         text1.destroy()
-                        text1 = this.add.text(512, 584, 'Una vez concluido este diálogo, \nel juego se acabará.\nNos vemos en próximas activiadades ', {
+                        text1 = this.add.text(512, 584, 'Una vez concluido este diálogo, \nel juego se acabará.\nNos vemos en próximas activiadades. ', {
                             fontSize: 38, color: '#ffffff',
                             stroke: '#000000', strokeThickness: 8,
                             align: 'center'
                         }).setOrigin(0.5);
                         this.input.once('pointerdown', () => {
-                            this.scene.start('GameOver');
+                            text1.destroy()
+                            text1 = this.add.text(512, 584, `Bueno, me olvidaba de la bicicleta.\nAquí te va:\nColor:${bici.color}\nModelo:${bici.modelo}\nUbicacion:${bici.ubicacion}`, {
+                                fontSize: 38, color: '#ffffff',
+                                stroke: '#000000', strokeThickness: 8,
+                                align: 'center'
+                            }).setOrigin(0.5);
+                            this.input.once('pointerdown', () => {
+                                this.scene.start('GameOver');
+                            });
                         });
                     });
                 });
@@ -58,4 +73,10 @@ export class Game extends Scene {
 
 
     }
+}
+
+async function getBicis(url) {
+    const response = await fetch(url)
+    const data = await response.json()
+    return data
 }
